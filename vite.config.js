1e9cef1bf295fe87471bf00295bfb87442617788
Ayaba6 <ramodgwende421@gmail.com>
2025-11-13 01:16:34 +0000

    Ajout du manifeste PWA, icônes, nom et logo modifiés

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'GB-Fleet',            // Nom complet affiché sur mobile
+        short_name: 'GB-Fleet',            // Nom court pour l’écran d’accueil
+        description: 'Gestion de flotte',
+        theme_color: '#0ea5e9',            // Couleur de la barre de statut
+        background_color: '#ffffff',       // Couleur de fond au lancement
+        display: 'standalone',             // Pour que l’app s’ouvre en mode standalone
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
